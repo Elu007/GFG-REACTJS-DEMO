cf3065c133be43e43ceee56e15d336a7ecf8cbe7
Elaf <skelaf2000@gmail.com>
2023-10-14 20:36:26 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, I am {process.env.REACT_APP_NAME}</h1>
+      <h1>I work in {process.env.REACT_APP_COMPANY}</h1>
+      <h2>Here we are hosting this website on {process.env.REACT_APP_HOSTING}</h2>
+    </div>
+  );
+}
+
+export default App;
